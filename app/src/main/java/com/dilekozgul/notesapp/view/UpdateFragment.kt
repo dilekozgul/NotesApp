8a0d240890651201b19cac12d60d71a6bc760a91
Dilek Özgül <dilekozgul9@gmail.com>
2021-11-23 13:33:36 +0300

    NotesApp

--- app/src/main/java/com/dilekozgul/notesapp/view/UpdateFragment.kt
+++ app/src/main/java/com/dilekozgul/notesapp/view/UpdateFragment.kt
@@ -24,6 +24,7 @@ class UpdateFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
     }
+    
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
